b2947c138dc862600e28cc82400a6557655c607f
Fac943 <149449123+Fac943@users.noreply.github.com>
2026-05-26 18:45:18 -0700

    added ADC files

--- ADC.c
+++ ADC.c
@@ -1,42 +1,168 @@
+/*
+ *******************************************************************************
+ * EE 329 A8 NUCLEO ADC & Current Source
+ *******************************************************************************
+ * @file           : ADC.c
+ * @brief          : Inits ADC1 in port A, functions to read data from PA0
+ * project         : EE 329 S'26 A8
+ * authors         : Facundo Soto-Wang & Samuel Weston.
+ * version         : 0.1
+ * date            : 260523
+ * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ * ADC WIRING
+ * PA0  put connetor pin -> relay NO to resistor node
+ *******************************************************************************
+ * Header format adapted from [Code Appendix by Kevin Vo] pg 5
+ */
 
 #include "ADC.h"
 
-
-void ADC_init(){
-RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;         // turn on clock for ADC
+volatile uint16_t ADC_sample = 0;
+volatile uint8_t ADC_DRDY = 0;
+/*-----------------------------------------------------------------------------
+ * function : ADC_init();
+ * INs      : none
+ * OUTs     : none
+ * action   : initializes ADC1 CH5 with Vref = 3.3V
+ * authors  : EE329 Lab Manual - A8 sample code - Modified by Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by main.c
+ *----------------------------------------------------------------------------*/
+void ADC_init(void){
+   RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;         // turn on clock for ADC
 // power up & calibrate ADC
-ADC123_COMMON->CCR |= (1 << ADC_CCR_CKMODE_Pos); // clock source = HCLK/1
-ADC1->CR &= ~(ADC_CR_DEEPPWD);             // disable deep-power-down
-ADC1->CR |= (ADC_CR_ADVREGEN);             // enable V regulator - see RM 18.4.6
-delay_us(20);                              // wait 20us for ADC to power up
-ADC1->DIFSEL &= ~(ADC_DIFSEL_DIFSEL_5);    // PA0=ADC1_IN5, single-ended
-ADC1->CR &= ~(ADC_CR_ADEN | ADC_CR_ADCALDIF); // disable ADC, single-end calib
-ADC1->CR |= ADC_CR_ADCAL;                  // start calibration
-while (ADC1->CR & ADC_CR_ADCAL) {;}        // wait for calib to finish
-// enable ADC
-ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
-ADC1->CR |= ADC_CR_ADEN;                   // enable ADC
-while(!(ADC1->ISR & ADC_ISR_ADRDY)) {;}    // wait for ADC Ready flag
-ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
-// configure ADC sampling & sequencing
-ADC1->SQR1  |= (5 << ADC_SQR1_SQ1_Pos);    // sequence = 1 conv., ch 5
-ADC1->SMPR1 |= (1 << ADC_SMPR1_SMP5_Pos);  // ch 5 sample time = 6.5 clocks
-ADC1->CFGR  &= ~( ADC_CFGR_CONT  |         // single conversion mode
+   ADC123_COMMON->CCR |= (1 << ADC_CCR_CKMODE_Pos); // clock source = HCLK/1
+   ADC1->CR &= ~(ADC_CR_DEEPPWD);             // disable deep-power-down
+   ADC1->CR |= (ADC_CR_ADVREGEN);             // enable V regulator - see RM 18.4.6
+   delay_us(20);                              // wait 20us for ADC to power up
+   ADC1->DIFSEL &= ~(ADC_DIFSEL_DIFSEL_5);    // PA0=ADC1_IN5, single-ended
+   ADC1->CR &= ~(ADC_CR_ADEN | ADC_CR_ADCALDIF); // disable ADC, single-end calib
+   ADC1->CR |= ADC_CR_ADCAL;                  // start calibration
+   while (ADC1->CR & ADC_CR_ADCAL) {;}        // wait for calib to finish
+   // enable ADC
+   ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
+   ADC1->CR |= ADC_CR_ADEN;                   // enable ADC
+   while(!(ADC1->ISR & ADC_ISR_ADRDY)) {;}    // wait for ADC Ready flag
+   ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
+   // configure ADC sampling & sequencing
+   ADC1->SQR1  |= (5 << ADC_SQR1_SQ1_Pos);    // sequence = 1 conv., ch 5
+   ADC1->SMPR1 |= (1 << ADC_SMPR1_SMP5_Pos);  // ch 5 sample time = 6.5 clocks
+   ADC1->CFGR  &= ~( ADC_CFGR_CONT  |         // single conversion mode
                   ADC_CFGR_EXTEN |         // h/w trig disabled for s/w trig
                   ADC_CFGR_RES   );        // 12-bit resolution
-// configure GPIO pin PA0
-RCC->AHB2ENR  |= (RCC_AHB2ENR_GPIOAEN);    // connect clock to GPIOA
-GPIOA->MODER  |= (GPIO_MODER_MODE0);       // analog mode for PA0 (set MODER last)
 
-ADC1->CR |= ADC_CR_ADSTART;                // start 1st conversion
+   // configure & enable ADC interrupt
+   ADC1->IER |= ADC_IER_EOCIE;                // enable end-of-conv interrupt
+   ADC1->ISR |= ADC_ISR_EOC;                  // set to clear EOC flag
+   NVIC->ISER[0] = (1<<(ADC1_2_IRQn & 0x1F)); // enable ADC interrupt service
+   __enable_irq();                            // enable global interrupts
+
+   //GPIO config
+   RCC->AHB2ENR  |= (RCC_AHB2ENR_GPIOAEN);    // AHB2 clock to GPIOA
+   GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL0); 			// clear alt function
+   GPIOA->AFR[0] |= (7 << GPIO_AFRL_AFSEL0_Pos); 	// SEL AF 7 for CH5 of ADC1
+   GPIOA->MODER  |= (GPIO_MODER_MODE0);       // analog mode
+
+   ADC1->CR |= ADC_CR_ADSTART;                // start 1st conversion
 }
 
+/*-----------------------------------------------------------------------------
+ * function : Conv_ADC();
+ * INs      : none
+ * OUTs     : 16 bit ADC voltage reading (mV)
+ * action   : reads ADC data register,
+ *            converts to voltage with calibrated slope and offset
+ * authors  : Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by ADC1_2_IRQHandler
+ *----------------------------------------------------------------------------*/
+uint16_t Conv_ADC(void){
+   uint16_t ADC_Data = ADC1->DR;              // Read data register
+   uint32_t milvolts = ((uint32_t)ADC_Data*VREF)/ADC_MAX;  //convert to mV
+   milvolts = ((M*milvolts)/1000);  //account for calibrated slope
+   milvolts = (milvolts + B);                  //account for offset
+   return (uint16_t)milvolts;
+}
 
-uint16_t Sample_angle(){
-   while(~(ADC1->ADC_ISR_EOC)){ 
-		uint16_t adcData = ADC1->DR;
-      ADC1->CR |= ADC_CR_ADSTART;					//start next conv
-	   return uint16_t adcData;	
+/*-----------------------------------------------------------------------------
+ * function : Get_Min();
+ * INs      : none
+ * OUTs     : 16b Min voltage of ADC sample array [20]
+ * action   : Finds min voltage in 20 element wide array
+ * authors  : Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by main.c
+ *----------------------------------------------------------------------------*/
+uint16_t Get_Min(uint16_t ADC_Samples[]){
+   uint16_t Min_volts = ADC_Samples[0];
+   for(uint8_t i = 1; i < 20; i++){
+      if(ADC_Samples[i] < Min_volts){
+         Min_volts = ADC_Samples[i];
+      }
+   }
+   return Min_volts;
+}
+
+/*-----------------------------------------------------------------------------
+ * function : Get_Avg();
+ * INs      : none
+ * OUTs     : 16b avg voltage of ADC sample array [20]
+ * action   : calculates avg voltage in 20 element wide array
+ * authors  : Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by main.c
+ *----------------------------------------------------------------------------*/
+uint16_t Get_Avg(uint16_t ADC_Samples[]){
+   uint32_t Sum_volts = 0;
+   uint16_t Avg_volts = 0;
+   for(uint8_t i = 0; i < 20; i++){
+   	Sum_volts += ADC_Samples[i];
+   }
+   Avg_volts = Sum_volts / 20;
+   return Avg_volts;
+}
+
+/*-----------------------------------------------------------------------------
+ * function : Get_Max();
+ * INs      : none
+ * OUTs     : 16b Max voltage of ADC sample array [20]
+ * action   : Finds max voltage in 20 element wide array
+ * authors  : Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by main.c
+ *----------------------------------------------------------------------------*/
+uint16_t Get_Max(uint16_t ADC_Samples[]){
+   uint16_t Max_volts = ADC_Samples[0];
+   for(uint8_t i = 1; i < 20; i++){
+      if(ADC_Samples[i] > Max_volts){
+         Max_volts = ADC_Samples[i];
+      }
+   }
+   return Max_volts;
+}
+
+/*-----------------------------------------------------------------------------
+ * function : ADC1_2_IRQHandler();
+ * INs      : none
+ * OUTs     : none
+ * action   : When data is ready, EOC flag is raised
+ * authors  : Facundo Soto-Wang
+ * version  : 0.1
+ * date     : 260523
+ * usage    : called by ADC1_2_IRQHandler
+ *----------------------------------------------------------------------------*/
+void ADC1_2_IRQHandler(void){
+	if(ADC1->ISR & ADC_ISR_EOC){
+		ADC_sample = Conv_ADC();   //get sample
+		ADC_DRDY = 1;              //set data ready flag
+	   ADC1->ISR = ADC_ISR_EOC;   //clear EOC flag
 	}
 }
-	   
--- ADC.h
+++ ADC.h
@@ -1,17 +1,40 @@
 /*
- * ADC.h
- *
- *  Created on: May 19, 2026
- *      Author: facun
+ *******************************************************************************
+ * EE 329 A8 NUCLEO ADC & Current Source
+ *******************************************************************************
+ * @file           : ADC.h
+ * @brief          : Header file for ADC.c
+ * authors         : Facundo Soto-Wang & Samuel Weston.
+ * version         : 0.1
+ * date            : 260526
+ * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * Header format adapted from [Code Appendix by Kevin Vo] pg 5
  */
+
+#include "stm32l4xx_hal.h"
+#include "delay.h"
 
 #ifndef INC_ADC_H_
 #define INC_ADC_H_
 
-void ADC_init(void);
-void Sample_Angle(void);
+#define VREF        3300    	// Reference voltage (mV)
+#define ADC_MAX     4095    	// 12-bit max (2^12 - 1)
+#define M           1000    	// slope of uncalibrated DAC (mV)
+#define B           0   	   // y intercept of uncalibrated DAC (mV)
 
-extern volatile uint16_t ADC_Samples [20];
-extern volatile uint16_t ADC_SampleCount;
+void ADC_init(void);
+uint16_t Conv_ADC(void);
+uint16_t Get_Min(uint16_t ADC_Samples[]);
+uint16_t Get_Avg(uint16_t ADC_Samples[]);
+uint16_t Get_Max(uint16_t ADC_Samples[]);
+void ADC1_2_IRQHandler(void);
+
+extern volatile uint16_t ADC_sample;
+extern volatile uint8_t ADC_DRDY;
+
 
 #endif /* INC_ADC_H_ */
